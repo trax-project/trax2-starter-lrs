9b5f91c8db3b04d3dd7b389770c83ada7c61c7ce
sfraysse <sebastien@fraysse.eu>
2021-12-08 17:27:49 +0100

    version display

--- services/trax/lrs/resources/js/app/contents/BrandFooter.vue
+++ services/trax/lrs/resources/js/app/contents/BrandFooter.vue
@@ -2,7 +2,7 @@
     <div>
         You <i class="tim-icons icon-heart text-primary"></i> TRAX LRS?
         <a href="http://traxlrs.com" target="_blank" rel="noopener">Support us!</a>
-        <p class="version mt-1">{{ version }}</p>
+        <span class="version ml-4">{{ version }}</span>
     </div>
 </template>
 
